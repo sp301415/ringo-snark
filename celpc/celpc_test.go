fcee384f4630a024ef1f526bc81ee71cc6e9d2f9
Intak Hwang <sp301415@gmail.com>
2025-03-15 02:26:36 +0000

    feat: Add benchmarks

--- celpc/celpc_test.go
+++ celpc/celpc_test.go
@@ -1,6 +1,8 @@
 package celpc_test
 
 import (
+	"fmt"
+	"math"
 	"math/big"
 	"testing"
 
@@ -10,8 +12,23 @@ import (
 )
 
 var (
-	params = celpc.ParamsLogN19LogP256.Compile()
+	params            = celpc.ParamsLogN19LogP256.Compile()
+	paramsLiteralList = []celpc.ParametersLiteral{
+		celpc.ParamsLogN19LogP256,
+		celpc.ParamsLogN21LogP256,
+		celpc.ParamsLogN23LogP256,
+		celpc.ParamsLogN25LogP256,
+	}
 )
+
+func TestParameters(t *testing.T) {
+	for _, paramsLiteral := range paramsLiteralList {
+		logN := int(math.Log2(float64(paramsLiteral.Degree)))
+		t.Run(fmt.Sprintf("Compile/LogN=%v", logN), func(t *testing.T) {
+			assert.NotPanics(t, func() { paramsLiteral.Compile() })
+		})
+	}
+}
 
 func TestEncoder(t *testing.T) {
 	ecd := celpc.NewEncoder(params)
@@ -101,4 +118,78 @@ func TestCommitment(t *testing.T) {
 
 		assert.Equal(t, valueReal, evalPf.Value)
 	})
+}
+
+func BenchmarkCELPC(b *testing.B) {
+	for _, paramsLiteral := range paramsLiteralList {
+		params := paramsLiteral.Compile()
+		logN := int(math.Log2(float64(params.Degree())))
+
+		ck := celpc.GenAjtaiCommitKey(params)
+		prover := celpc.NewProver(params, ck)
+		verifier := celpc.NewVerifier(params, ck)
+		oracle := celpc.NewRandomOracle(params)
+
+		v := bigring.BigPoly{Coeffs: make([]*big.Int, params.Degree())}
+		for i := 0; i < v.Degree(); i++ {
+			v.Coeffs[i] = big.NewInt(0)
+			oracle.SampleModAssign(v.Coeffs[i])
+		}
+
+		com := celpc.NewCommitment(params, params.Degree())
+		open := celpc.NewOpening(params, params.Degree())
+		openPf := celpc.NewOpeningProof(params)
+		evalPf := celpc.NewEvaluationProof(params)
+
+		b.Run(fmt.Sprintf("Commit/LogN=%v", logN), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				prover.CommitAssign(v, com, open)
+			}
+		})
+
+		b.Run(fmt.Sprintf("CommitParallel/LogN=%v", logN), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				prover.CommitParallelAssign(v, com, open)
+			}
+		})
+
+		b.Run(fmt.Sprintf("ProveOpening/LogN=%v", logN), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				prover.ProveOpeningAssign([]celpc.Commitment{com}, []celpc.Opening{open}, openPf)
+			}
+		})
+
+		b.Run(fmt.Sprintf("ProveOpeningParallel/LogN=%v", logN), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				prover.ProveOpeningParallelAssign([]celpc.Commitment{com}, []celpc.Opening{open}, openPf)
+			}
+		})
+
+		x := big.NewInt(0)
+		oracle.SampleModAssign(x)
+
+		b.Run(fmt.Sprintf("Evaluate/LogN=%v", logN), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				prover.EvaluateAssign(x, open, evalPf)
+			}
+		})
+
+		b.Run(fmt.Sprintf("EvaluateParallel/LogN=%v", logN), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				prover.EvaluateParallelAssign(x, open, evalPf)
+			}
+		})
+
+		b.Run(fmt.Sprintf("VerifyOpeningProof/LogN=%v", logN), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				verifier.VerifyOpeningProof([]celpc.Commitment{com}, openPf)
+			}
+		})
+
+		b.Run(fmt.Sprintf("VerifyEvaluation/LogN=%v", logN), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				verifier.VerifyEvaluation(x, com, evalPf)
+			}
+		})
+	}
 }
